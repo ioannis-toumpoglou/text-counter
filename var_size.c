f017f168ea8dcd30dcfa51ad0f57852aa76f00cb
Giannis Toumboglou <32466180+toumb@users.noreply.github.com>
2018-05-25 00:16:22 +0300

    Delete var_size.c

--- var_size.c
+++
@@ -1,6 +0,0 @@
-main() {
-    printf("Type int is of size %d bytes.\n", sizeof(int));
-    printf("Type char is of size %d bytes.\n", sizeof(char));
-    printf("Type long is of size %d bytes.\n", sizeof(long));
-    printf("Type double is of size %d bytes.\n", sizeof(double));
-}
