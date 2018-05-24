34e360e37757b74c76255ebd03119e3690b1a491
Giannis Toumboglou <32466180+toumb@users.noreply.github.com>
2018-05-25 00:16:08 +0300

    Add files via upload

---
+++ var_size.c
@@ -0,0 +1,6 @@
+main() {
+    printf("Type int is of size %d bytes.\n", sizeof(int));
+    printf("Type char is of size %d bytes.\n", sizeof(char));
+    printf("Type long is of size %d bytes.\n", sizeof(long));
+    printf("Type double is of size %d bytes.\n", sizeof(double));
+}
